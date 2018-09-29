0b8903ea6092ce4cc5e5002287c4b6a0cf21311b
Jahn16 <Jahn16@users.noreply.github.com>
2018-09-29 13:28:49 -0300

    Initial commit

---
+++ js/aprender.js
@@ -0,0 +1,10 @@
+let aprenderEl = document.querySelector("#aprender");
+aprenderEl.addEventListener('click',function(){
+	limpa();
+
+	document.querySelector("#opcoes").innerHTML = "<h1>Aprender qual linguagem?</h1>;"
+	
+	document.querySelector("#opcoes").innerHTML += "<button class='btn btn-outline-primary' id='css'>CSS</button>";
+	document.querySelector("#opcoes").innerHTML += "<button class='btn btn-outline-success' id='js'>JavaScript</button>";
+	document.querySelector("#opcoes").innerHTML += "<button class='btn btn-outline-danger' id='backend'>BackEnd</button>";
+})
