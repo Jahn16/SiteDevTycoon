29baa28b9d1abe3b0fbad4085f10e3a231a4a436
Jahn16 <Jahn16@users.noreply.github.com>
2018-09-30 12:38:10 -0300

    1.0.7

--- js/desenvolver.js
+++ js/desenvolver.js
@@ -13,6 +13,7 @@ let teclado = new Audio('audio/sons/teclado.mp3');
 let beep = new Audio('audio/sons/beep.wav');
 let blop = new Audio('audio/sons/blop.mp3');
 let caixa = new Audio('audio/sons/dinheiro.mp3');
+let loading = new Audio('audio/sons/loading.mp3');
 $(document).ready(function() {
 	if (!localStorage.getItem('dinheiro')) {
 		localStorage.setItem('dinheiro',1000);
@@ -271,8 +272,9 @@ $(document).ready(function() {
 						
 						
    			 		barra(0);
-   			 		
+   			 			loading.play();
 						setTimeout(function() {
+							loading.stop();
    							notadosite(nota,ganho,nome);
    							$('#dinheiro').html('$'+localStorage.getItem('dinheiro'));
 						}, 10300);
@@ -449,7 +451,9 @@ function calculonota(nota){
 	return num;
 }
 function barra(c){
+	
 	if(c<=100){
+		loading.play();
 	$('#progresso').html('<div class="progress" ><div class="progress-bar progress-bar-striped" role="progressbar" style="width: '+c+'%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div></div>');
 	 $("#modal1").modal({
             backdrop: 'static',
@@ -458,7 +462,7 @@ function barra(c){
 	}
 	if(c==100){
 		 $('#modal1').modal('toggle');
-			
+		 loading.pause();
 		
 	}
 	setTimeout(function() {
