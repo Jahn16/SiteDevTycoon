3a5c489fd2019f23e1b69175648a4565231f713d
Jahn16 <Jahn16@users.noreply.github.com>
2018-09-30 18:57:44 -0300

    2.0.01

--- js/aprender.js
+++ js/aprender.js
@@ -1,8 +1,7 @@
-let aprenderEl = document.querySelector("#aprender");
-aprenderEl.addEventListener('click',function(){
+$('#opcoes').on("click", "#aprender", function() {
 	limpa();
 
-	document.querySelector("#opcoes").innerHTML = "<h1>Aprender qual linguagem?</h1>;"
+	document.querySelector("#opcoes").innerHTML = "<h1>Aprender qual linguagem?</h1>"
 	
 	document.querySelector("#opcoes").innerHTML += "<button class='btn btn-outline-primary' id='css'>CSS</button>";
 	document.querySelector("#opcoes").innerHTML += "<button class='btn btn-outline-success' id='js'>JavaScript</button>";
