c12c593ac5fc36e81b0350e5bad4ecb19538949b
Luiza Viana <luizavianabh@gmail.com>
2018-09-30 22:55:26 -0300

    menu musica prototipo

---
+++ js/toggle.js
@@ -0,0 +1,20 @@
+let navStatus = false;
+
+let nav = function() {
+	let getSidebar = document.querySelector(".nav-sidebar");
+	let getSidebarUl = document.querySelector(".nav-sidebar ul");
+
+	if(navStatus === false){
+
+		getSidebarUl.style.visibility = "visible";
+		getSidebar.style.width = "274px";
+
+	}
+	if(navStatus === true){
+
+
+	}
+
+
+
+}
